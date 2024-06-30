36b98dcd237142e986d2039b898b5ef5f75eefd3
Tarush-Goyal <tarushg2000@gmail.com>
2024-06-30 23:43:38 +0530

    changed react version

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -1,7 +1,6 @@
-// @flow strict
-
 import { experiences } from "@/utils/data/experience";
 import Image from "next/image";
+import Link from "next/link";
 import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
@@ -19,7 +18,7 @@ function Projects() {
       />
 
       <div className="flex justify-center my-5 lg:py-8">
-        <div className="flex  items-center">
+        <div className="flex items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
           <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-xl rounded-md">
             Projects
@@ -38,45 +37,47 @@ function Projects() {
 
           <div>
             <div className="flex flex-col gap-6">
-              {
-                experiences.map(experience => (
-                  <GlowCard key={experience.id} identifier={`experience-${experience.id}`}>
-                    <div className="p-3 relative">
-                      <Image
-                        src="/blur-23.svg"
-                        alt="Hero"
-                        width={1080}
-                        height={200}
-                        className="absolute bottom-0 opacity-80"
-                      />
-                      <div className="flex justify-center">
-                        <p className="text-xs sm:text-sm text-[#16f2b3]">
-                          {experience.duration}
-                        </p>
-                      </div>
-                      <div className="flex items-center gap-x-8 px-3 py-5">
-                        <div className="text-violet-500  transition-all duration-300 hover:scale-125">
-                          <BsPersonWorkspace size={36} />
-                        </div>
-                        <div>
-                          <p className="text-base sm:text-xl mb-2 font-medium uppercase">
-                            {experience.title}
-                          </p>
-                          <p className="text-sm sm:text-base">
-                            {experience.company}
+              {experiences.map(experience => (
+                <Link key={experience.id} href={`${experience.url}`} legacyBehavior>
+                  <a>
+                    <GlowCard identifier={`experience-${experience.id}`}>
+                      <div className="p-3 relative">
+                        <Image
+                          src="/blur-23.svg"
+                          alt="Hero"
+                          width={1080}
+                          height={200}
+                          className="absolute bottom-0 opacity-80"
+                        />
+                        <div className="flex justify-center">
+                          <p className="text-xs sm:text-sm text-[#16f2b3]">
+                            {experience.duration}
                           </p>
                         </div>
+                        <div className="flex items-center gap-x-8 px-3 py-5">
+                          <div className="text-violet-500 transition-all duration-300 hover:scale-125">
+                            <BsPersonWorkspace size={36} />
+                          </div>
+                          <div>
+                            <p className="text-base sm:text-xl mb-2 font-medium uppercase">
+                              {experience.title}
+                            </p>
+                            <p className="text-sm sm:text-base">
+                              {experience.company}
+                            </p>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </GlowCard>
-                ))
-              }
+                    </GlowCard>
+                  </a>
+                </Link>
+              ))}
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Projects;
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,8 +1,8 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "CommerceIQ",
-    duration: "(Feb 2022 - Present)"
+    title: 'Sahyog (MERN Stack)',
+    company: "Sahyog provides a platform for NGOs to connect with donors. It acts as an aggregator where people can donate items such as stationary, clothes etc, and NGOs can pickup the stuff from the specified address and further donate it to the needy.",
+    url: "https://github.com/Tarush-Goyal/Sahyog-Frontend"
   }
 ]
